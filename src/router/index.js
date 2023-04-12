e73b89d7e9b55f4dc08f37b1185a853a385d6f34
AlexeyNelyubov <ANelyubov93@gmail.com>
2023-04-12 12:08:54 +0300

    add dist

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,22 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-// import IndexPage from "@/views/IndexView.vue";
-// import AboutUsPage from "@/views/AboutUsVeiw.vue";
-// import PetPage from "@/views/PetView.vue";
-// import ProfilePage from "@/views/ProfileView.vue";
-// import SignUpPage from "@/views/SignUpView.vue";
-// import SignInPage from "@/views/SignInView.vue";
-// import PasswordRecoveryPage from "@/views/PasswordRecovery.vue";
-// import CreateNewPetCardPage from "@/views/CreateNewPetCard.vue";
+import IndexPage from "@/views/IndexView.vue";
+import AboutUsPage from "@/views/AboutUsVeiw.vue";
+import PetPage from "@/views/PetView.vue";
+import ProfilePage from "@/views/ProfileView.vue";
+import SignUpPage from "@/views/SignUpView.vue";
+import SignInPage from "@/views/SignInView.vue";
+import PasswordRecoveryPage from "@/views/PasswordRecovery.vue";
+import CreateNewPetCardPage from "@/views/CreateNewPetCard.vue";
 
-const IndexPage = () => import("../views/IndexView.vue");
-const AboutUsPage = () => import("../views/AboutUsVeiw.vue");
-const PetPage = () => import("@/views/PetView.vue");
-const ProfilePage = () => import("@/views/ProfileView.vue");
-const SignUpPage = () => import("@/views/SignUpView.vue");
-const SignInPage = () => import("@/views/SignInView.vue");
-const PasswordRecoveryPage = () => import("@/views/PasswordRecovery.vue");
-const CreateNewPetCardPage = () => import("@/views/CreateNewPetCard.vue");
+// const IndexPage = () => import("../views/IndexView.vue");
+// const AboutUsPage = () => import("../views/AboutUsVeiw.vue");
+// const PetPage = () => import("@/views/PetView.vue");
+// const ProfilePage = () => import("@/views/ProfileView.vue");
+// const SignUpPage = () => import("@/views/SignUpView.vue");
+// const SignInPage = () => import("@/views/SignInView.vue");
+// const PasswordRecoveryPage = () => import("@/views/PasswordRecovery.vue");
+// const CreateNewPetCardPage = () => import("@/views/CreateNewPetCard.vue");
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
