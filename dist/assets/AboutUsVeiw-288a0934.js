3aa1071fc3e97f24589959e4a7489c69dc1986d3
AlexeyNelyubov <ANelyubov93@gmail.com>
2023-04-14 17:20:11 +0300

    add dist

--- dist/assets/AboutUsVeiw-288a0934.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-193aea42.js";const r={};function t(c,_){return"ABOUTUS"}const o=e(r,[["render",t]]);export{o as default};
---
+++ dist/assets/AboutUsVeiw-c54eb06b.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-d6bb07ff.js";const r={};function t(c,_){return"ABOUTUS"}const o=e(r,[["render",t]]);export{o as default};
